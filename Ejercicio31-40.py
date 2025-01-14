ce9ba112cb841ae8712461b5d06a3d5475026849
JNaor12 <guannaor@gmail.com>
2025-01-14 14:01:34 +0100

    Ejercicio 40

--- Ejercicio31-40.py
+++ Ejercicio31-40.py
@@ -29,26 +29,86 @@
 age = 2028
 
 if (age % 4 == 0 and age % 100 != 0) or (age % 400 == 0):
-    print('Es año bisiesto')
+    print('Ejercicio33: Es año bisiesto')
 else:
-    print('No es año bisiesto')
+    print('Ejercicio33: No es año bisiesto')
 
 """ Verifica si una cadena es mayor o igual a 10 caracteres """
 
 cadena = "hola que tal"
 
 if len(cadena) > 10:
-    print('Más de 10 carácteres')
+    print('Ejercicio34: Más de 10 carácteres')
 elif len(cadena) < 10:
-    print('Menos de 10 caracteres')
+    print('Ejercicio34: Menos de 10 caracteres')
 else:
-    print('Tiene 10 carácteres')
+    print('Ejercicio34: Tiene 10 carácteres')
 
 """ Comprueba si un número está en el rango de 1 y 100 """
 
 num = 88
 
 if 1 <= num <= 100:
-    print('Está entre 1 y 100')
+    print('Ejercicio35: Está entre 1 y 100')
 else:
-    print('No está entre 1 y 100')
+    print('Ejercicio35: No está entre 1 y 100')
+
+""" Pide un carácter y Determina si es un vocal """
+
+caracter = input("Escríbe un carácter: ")
+
+vocales = ['a', 'e', 'i', 'o' ,'u']
+
+if caracter.lower() in vocales:
+    print("Ejercicio36: Es una vocal")
+else:
+    print("Ejercicio36: No es una vocal")
+
+""" Calcula el máximo de tres números """
+
+num1 = 8
+num2 = 5
+num3 = 9
+
+maximo = max(num1,num2,num3)
+
+print(f'Ejercicio37: El número mas alto es: {maximo}')
+
+""" Determina si un número es divisible entre 5 y 7 """
+
+num = 35
+
+if num % 5 == 0 and num % 7 == 0:
+    print('Ejercicio38: Es divisible entre 5 y 7')
+else:
+    print('Ejercicio38: No es divisible entre 5 y 7')
+
+""" Verifica si la pablabra ingresada es "python" """
+
+palabra = input('Escribe la palabra: python -> ')
+
+if palabra.lower() == 'python':
+    print('Ejercicio39: Gracias!')
+else:
+    print('Ejercicio39: Por que no me haces caso?' )
+
+""" Calcula el ICM e intrepretalo """
+
+peso = 75
+altura = 1.75
+
+imc = peso / (altura**2)
+print(f'Ejercicio40: {imc}')
+
+if imc < 18.5:
+    print('Bajo peso')
+elif imc < 25:
+    print('Peso Normal')
+elif  imc < 30:
+    print('Sobrepeso')
+elif imc < 35:
+    print('Obesidad grado I')
+elif imc < 40:
+    print('Obesidad grado II')
+else:
+    print('Obesidad grado III')
